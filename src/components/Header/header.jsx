a87ad52b5523003f3b20ce245895b7e6ece03cf5
Pedro Leonardo <leo.dev.ny@gmail.com>
2024-11-23 16:10:21 -0300

    adaptei o menu pra mobile

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,16 +4,32 @@ function Header(){
   function btnModo(){
     document.body.classList.toggle("lightmode")
   }
+  function menu(){
+    
+    if(nav.style.display == 'flex'){
+      nav.style.display = 'none'
+    }else{
+      nav.style.display = 'flex'
+    }
+  }
   return(
     <>
       <header>
-        <nav>
+        <h2>NY</h2>
+        <nav id='nav'>
           <a class="links nav" href="#home">Home</a>
           <a class="links nav" href="#">sobre</a>
           <a  class="links nav"href="#projetos">Projetos</a>
           <a class="links nav" href="#">Contatos</a>
-          <button onClick={btnModo} className=''><div></div></button>
         </nav>
+        <div className='btns'>
+          <button id="mode"onClick={btnModo} className=''><div></div></button>
+          <button onClick={menu} id="menu">
+            <div className="items-menu"></div>
+            <div className="items-menu"></div>
+            <div className="items-menu"></div>
+          </button>
+        </div>
       </header>
     </>
   )
