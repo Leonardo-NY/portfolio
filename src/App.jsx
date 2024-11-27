1e402d39db4ef684e974e0c1e23554e7805da1f3
Pedro Leonardo <leo.dev.ny@gmail.com>
2024-11-27 18:27:30 -0300

    iniciei o footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header/header'
 import Home from './components/Home/home'
+import Contato from './components/Contatos/contato'
 function App() {
   return (
     <>
       <Header/>
       <Home/>
+      <Contato/>
     </>
   )
 }
---
+++ src/components/Contatos/contato.jsx
@@ -0,0 +1,19 @@
+import './contato.css'
+function Contato(){
+  return(
+    <>
+      <footer>
+        <p>Precisa de um Desenvolvedor?</p>
+        <h2>Vamos trabalhar juntos.</h2>
+        <section className='contatos'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <p>&copy;2024 todos os direitos reservados.</p>
+
+      </footer>
+    </>
+  )
+}
+export default Contato
