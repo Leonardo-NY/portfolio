cbdaa7b98c227b6fcbde8279e33736e740906f6a
Pedro Leonardo <leo.dev.ny@gmail.com>
2024-11-28 11:41:15 -0300

    por algum motivo nao aparece os iconhes

--- src/components/Contatos/contato.jsx
+++ src/components/Contatos/contato.jsx
@@ -6,9 +6,18 @@ function Contato(){
         <p>Precisa de um Desenvolvedor?</p>
         <h2>Vamos trabalhar juntos.</h2>
         <section className='contatos'>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <p>Instagran</p>
+            <img src="../../assets/images/i-insta.png" alt="" />
+          </div>
+          <div>
+            <p>Linkedin</p>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <p>E-mail</p>
+            <img src="" alt="" />
+          </div>
         </section>
         <p>&copy;2024 todos os direitos reservados.</p>
 
