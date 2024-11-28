6020d39679d58b0b2abd3825b8c96d3b62f61b6f
Pedro Leonardo <leo.dev.ny@gmail.com>
2024-11-28 16:46:19 -0300

    finalizaei a area sobre

--- src/components/Sobre/sobre.jsx
+++ src/components/Sobre/sobre.jsx
@@ -3,8 +3,14 @@ function Sobre(){
   return(
     <>
       <section id='sobre'>
-        <p id='p-sobre-um'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vero minus dolorum necessitatibus culpa nihil praesentium rerum. Quidem, inventore, quos vitae a totam laborum fugiat voluptas, vel ad autem cum!</p>
-        <p id='p-sobre-dois'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum repellat itaque blanditiis possimus numquam praesentium deleniti, nam sit totam veniam laborum eius, in dolores ipsam sint corrupti perspiciatis aliquam? Quasi.</p>
+        <div className='div-sobre'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci expedita assumenda blanditiis harum libero rerum tempore animi sunt vel aperiam, itaque velit nemo commodi numquam, voluptatem veniam quasi, ratione dolorem?</p>
+          <span>01001000</span>
+        </div>
+        <div className='div-sobre'>
+          <button><p>Curriculo</p></button>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dignissimos maxime tempora suscipit nemo accusantium, maiores consequuntur deleniti debitis fugit, deserunt voluptas, nam autem ipsam esse. Quisquam rem voluptatibus error?</p>
+        </div>
       </section>
     </>
   )
